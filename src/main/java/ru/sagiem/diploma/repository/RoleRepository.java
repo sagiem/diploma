1d1475d38fb4a3e11f10fea5946648a298e0ca6b
Maksim Kashin <sagiems@gmail.com>
2023-02-02 10:37:02 +0600

    Roleservice

--- src/main/java/ru/sagiem/diploma/repository/RoleRepository.java
+++ src/main/java/ru/sagiem/diploma/repository/RoleRepository.java
@@ -1,7 +1,9 @@
 package ru.sagiem.diploma.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import ru.sagiem.diploma.model.Role;
 
 @Repository
-public interface RoleRepository {
+public interface RoleRepository extends CrudRepository<Role, Long> {
 }
--- src/main/java/ru/sagiem/diploma/service/impl/RoleServiceImpl.java
+++ src/main/java/ru/sagiem/diploma/service/impl/RoleServiceImpl.java
@@ -1,35 +1,21 @@
 package ru.sagiem.diploma.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 import ru.sagiem.diploma.model.Role;
+import ru.sagiem.diploma.repository.RoleRepository;
 import ru.sagiem.diploma.service.interf.RoleService;
 
 import java.util.List;
 
 @Service
-public class RoleServiceImpl implements RoleService {
+public class RoleServiceImpl extends AbstractCRUDService<Role, Long> implements RoleService {
+
+    @Autowired
+    RoleRepository roleRepository;
     @Override
-    public void create(Role entity) {
-
-    }
-
-    @Override
-    public Role findById(Long id) {
-        return null;
-    }
-
-    @Override
-    public List<Role> findAll() {
-        return null;
-    }
-
-    @Override
-    public Role update(Role entity) {
-        return null;
-    }
-
-    @Override
-    public void delete(Role entity) {
-
+    CrudRepository<Role, Long> getRepository(){
+        return roleRepository;
     }
 }
